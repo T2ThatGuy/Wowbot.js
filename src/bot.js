2fe1d1ee41e6ec80c34ed8c49fa240e8caa289c5
T2ThatGuy <66382380+T2ThatGuy@users.noreply.github.com>
2021-09-15 21:03:41 +0100

    Fixed events not being loaded

--- src/bot.js
+++ src/bot.js
@@ -10,10 +10,17 @@ client.commands = new Collection();
 require('dotenv').config();
 const fs = require('fs');
 
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
 
 module.exports = (async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+
     client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
     
     client.login(process.env.DISCORD_TOKEN);
 })
